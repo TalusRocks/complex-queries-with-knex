a017123973b907352f071de303e83263af4799fb
Wes Reid <bwreid@gmail.com>
2017-10-27 15:29:01 -0700

    allow for multiple queries on the page

--- queries.js
+++ queries.js
@@ -2,9 +2,15 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[env]
 const knex = require('knex')(config)
 
-const query = knex('computers')
+const queries = {}
 
-query.then(result => {
+queries.allComputers = knex('computers')
+queries.allUsers = knex('users')
+queries.allDirectories = knex('directories')
+queries.allFiles = knex('files')
+
+// Test out a specific query here!
+queries.allComputers.then(result => {
   console.log(result)
   knex.destroy()
 }).catch(() => {
